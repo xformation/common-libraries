d2b795a1d4aa1b2dae34a881cd396f101e386fe5
sharmanju <sharmanju80@gmail.com>
2018-11-04 01:44:38 +0530

    Update dynamic mappings to parse date fields

--- src/main/java/com/synectiks/commons/entities/SourceMapping.java
+++ src/main/java/com/synectiks/commons/entities/SourceMapping.java
@@ -11,16 +11,42 @@ import org.codehaus.jettison.json.JSONObject;
  */
 public class SourceMapping {
 
+	public static String[] dtFormats = {
+			"yyyyMMdd",
+			"yyyyMMddHHmmss",
+			"yyyyMMddHHmmssZ",
+			"yyyy-MM-dd",
+			"yyyy-MM-dd HH:mm:ss.SSSZ",
+			"yyyy-MM-dd HH:mm:ss.SSS",
+			"yyyy-MM-dd HH:mm:ss",
+			"dd-MM-yyyy",
+			"dd-MM-yyyy HH:mm:ss.SSSZ",
+			"dd-MM-yyyy HH:mm:ss.SSS",
+			"dd-MM-yyyy HH:mm:ss",
+			"yyyy/MM/dd",
+			"yyyy/MM/dd HH:mm:ss.SSSZ",
+			"yyyy/MM/dd HH:mm:ss.SSS",
+			"yyyy/MM/dd HH:mm:ss",
+			"dd/MM/yyyy",
+			"dd/MM/yyyy HH:mm:ss.SSSZ",
+			"dd/MM/yyyy HH:mm:ss.SSS",
+			"dd/MM/yyyy HH:mm:ss",
+			"MMM dd, yyyy",
+			"MMM dd, yyyy HH:mm:ss"
+	};
+
 	public static JSONObject getSourceEntityMapping() {
 		StringBuilder srcDynMap = new StringBuilder();
 		srcDynMap.append("{");
 		srcDynMap.append("	\"_timestamp\": {");
 		srcDynMap.append("		\"enabled\": \"true\"");
-		srcDynMap.append("	},");
-		srcDynMap.append("	\"dynamic_templates\": [");
+		srcDynMap.append("	}, ");
+		srcDynMap.append("\"dynamic_templates\": [");
 		srcDynMap.append(getNestedMapping());
 		srcDynMap.append(", ");
 		srcDynMap.append(getIdMappingSyntax());
+		srcDynMap.append(", ");
+		srcDynMap.append(getDateMappingSyntax());
 		srcDynMap.append("	]");
 		srcDynMap.append("}");
 		JSONObject mappings = null;
@@ -32,6 +58,45 @@ public class SourceMapping {
 		return mappings;
 	}
 
+	private static String getDynamicDateFormats() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\"dynamic_date_formats\": [ ");
+		sb.append(getDFs(", ", true));
+		sb.append("]");
+		return sb.toString();
+	}
+
+	private static String getDFs(String sep, boolean quotes) {
+		StringBuilder sb = new StringBuilder();
+		boolean bFrst = true;
+		for (String df : dtFormats) {
+			if (bFrst) {
+				bFrst = false;
+			} else {
+				sb.append(sep);
+			}
+			sb.append((quotes) ? "\"" + df + "\"": df);
+		}
+		return sb.toString();
+	}
+
+	private static String getDateMappingSyntax() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("	\"dates_ignore_malformed\": {");
+		sb.append("		\"match_mapping_type\": \"date|string\",");
+		sb.append("		\"match_pattern\": \"regex\",");
+		sb.append("		\"path_match\": \".*(date|Date|_at|_At|_AT|dob|DOB|Dob)$\",");
+		sb.append("		\"mapping\": {");
+		sb.append("			\"type\": \"date\",");
+		sb.append("			\"ignore_malformed\": true,");
+		sb.append("			\"format\": \"" + getDFs(" || ", false) + "\"");
+		sb.append("		}");
+		sb.append("	}");
+		sb.append("}");
+		return sb.toString();
+	}
+
 	private static String getNestedMapping() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("{");
@@ -39,7 +104,9 @@ public class SourceMapping {
 		sb.append("		\"match_mapping_type\": \"object\",");
 		sb.append("		\"mapping\": {");
 		sb.append("			\"type\": \"nested\",");
-		sb.append("			\"dynamic\": true");
+		sb.append("			\"dynamic\": true,");
+		sb.append("			\"date_detection\": true,");
+		sb.append("			" + getDynamicDateFormats());
 		sb.append("		}");
 		sb.append("	}");
 		sb.append("}");
