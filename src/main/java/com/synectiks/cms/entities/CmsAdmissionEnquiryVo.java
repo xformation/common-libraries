485bb201baa9ea122cbd9b3b77ad04c01b1faafa
manojkrjoshi <44704973+manojkrjoshi@users.noreply.github.com>
2019-12-27 00:37:53 +0530

    vo added to common libraries

--- src/main/java/com/synectiks/cms/entities/CmsAdmissionEnquiryVo.java
+++ src/main/java/com/synectiks/cms/entities/CmsAdmissionEnquiryVo.java
@@ -1,446 +1,231 @@
 package com.synectiks.cms.entities;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.synectiks.cms.entities.enumeration.CourseEnum;
-import com.synectiks.cms.entities.enumeration.EnquiryStatus;
-import com.synectiks.cms.entities.enumeration.Gender;
-import com.synectiks.cms.entities.enumeration.ModeOfEnquiry;
-
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Objects;
+import java.util.List;
 
-public class CmsAdmissionEnquiryVo implements Serializable {
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
-	private Long totalAdmissions = 0L;
-	private Long totalFollowup = 0L;
-	private Long totalDeclined = 0L;
-	private Long totalConverted = 0L;
-	private Long id;
-	private String studentName;
-	private String studentMiddleName;
-	private String studentLastName;
-	private String fatherName;
-	private String fatherMiddleName;
-	private String fatherLastName;
-	private String motherName;
-	private String motherMiddleName;
-	private String motherLastName;
-	private String contactNumber;
-	private String alternateMobileNumber;
-	@JsonSerialize(using = LocalDateSerializer.class)
-	@JsonDeserialize(using = LocalDateDeserializer.class)
-	private LocalDate dateOfBirth;
-	private String email;
-	private Gender sex;
-	private String comments;
-	private CourseEnum courseApplyingFor;
-	private String highestQualification;
-	private ModeOfEnquiry modeOfEnquiry;
-	private EnquiryStatus status;
-	private String description;
-	@JsonSerialize(using = LocalDateSerializer.class)
-	@JsonDeserialize(using = LocalDateDeserializer.class)
-	private LocalDate enquiryDate;
-	@JsonSerialize(using = LocalDateSerializer.class)
-	@JsonDeserialize(using = LocalDateDeserializer.class)
-	private LocalDate updatedOn;
-	private String updatedBy;
-	private Branch branch;
-	private Department department;
-	private Batch batch;
-	private State state;
-	private City city;
-	private Country country;
 
-	private String strDateOfBirth;
-	private String strEnquiryDate;
-	private String strUpdatedOn;
 
-	public Long getTotalAdmissions() {
-		return totalAdmissions;
-	}
+public class CmsAdmissionEnquiryVo extends CmsCommonVo implements Serializable{
 
-	public void setTotalAdmissions(Long totalAdmissions) {
-		this.totalAdmissions = totalAdmissions;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public Long getTotalFollowup() {
-		return totalFollowup;
-	}
-
-	public void setTotalFollowup(Long totalFollowup) {
-		this.totalFollowup = totalFollowup;
-	}
-
-	public Long getTotalDeclined() {
-		return totalDeclined;
-	}
-
-	public void setTotalDeclined(Long totalDeclined) {
-		this.totalDeclined = totalDeclined;
-	}
-
-	public Long getTotalConverted() {
-		return totalConverted;
-	}
-
-	public void setTotalConverted(Long totalConverted) {
-		this.totalConverted = totalConverted;
-	}
-
+    private Long id;
+    private String studentName;
+    private String studentMiddleName;
+    private String studentLastName;
+    private String cellPhoneNo;
+    private String landLinePhoneNo;
+    private String emailId;
+    
+    @JsonSerialize(using = ToStringSerializer.class)
+    private LocalDate dateOfBirth;
+    
+    private String gender;
+    private String highestQualification;
+    private String modeOfEnquiry;
+    
+    @JsonSerialize(using = ToStringSerializer.class)
+    private LocalDate enquiryDate;
+    
+    private String comments;
+    private Long branchId;
+    private Long departmentId;
+    private Long courseId;
+    private Long semesterId;
+    private Long batchId;
+    private Long stateId;
+    private Long cityId;
+    private Long academicYearId;
+    private String enquiryStatus;
+    
+    private String strDateOfBirth;
+    private String strEnquiryDate;
+    
+    private List<String> genderList;
+    private List<String> modeOfEnquiryList;
+    private List<String> enquiryStatusList;
+    private String transactionSource;
+    private String sourceOfApplication;
+    
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getStudentName() {
 		return studentName;
 	}
-
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
-
 	public String getStudentMiddleName() {
 		return studentMiddleName;
 	}
-
 	public void setStudentMiddleName(String studentMiddleName) {
 		this.studentMiddleName = studentMiddleName;
 	}
-
 	public String getStudentLastName() {
 		return studentLastName;
 	}
-
 	public void setStudentLastName(String studentLastName) {
 		this.studentLastName = studentLastName;
 	}
-
-	public String getFatherName() {
-		return fatherName;
+	public String getCellPhoneNo() {
+		return cellPhoneNo;
 	}
-
-	public void setFatherName(String fatherName) {
-		this.fatherName = fatherName;
+	public void setCellPhoneNo(String cellPhoneNo) {
+		this.cellPhoneNo = cellPhoneNo;
 	}
-
-	public String getFatherMiddleName() {
-		return fatherMiddleName;
+	public String getLandLinePhoneNo() {
+		return landLinePhoneNo;
 	}
-
-	public void setFatherMiddleName(String fatherMiddleName) {
-		this.fatherMiddleName = fatherMiddleName;
+	public void setLandLinePhoneNo(String landLinePhoneNo) {
+		this.landLinePhoneNo = landLinePhoneNo;
 	}
-
-	public String getFatherLastName() {
-		return fatherLastName;
+	public String getEmailId() {
+		return emailId;
 	}
-
-	public void setFatherLastName(String fatherLastName) {
-		this.fatherLastName = fatherLastName;
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
 	}
-
-	public String getMotherName() {
-		return motherName;
-	}
-
-	public void setMotherName(String motherName) {
-		this.motherName = motherName;
-	}
-
-	public String getMotherMiddleName() {
-		return motherMiddleName;
-	}
-
-	public void setMotherMiddleName(String motherMiddleName) {
-		this.motherMiddleName = motherMiddleName;
-	}
-
-	public String getMotherLastName() {
-		return motherLastName;
-	}
-
-	public void setMotherLastName(String motherLastName) {
-		this.motherLastName = motherLastName;
-	}
-
-	public String getContactNumber() {
-		return contactNumber;
-	}
-
-	public void setContactNumber(String contactNumber) {
-		this.contactNumber = contactNumber;
-	}
-
-	public String getAlternateMobileNumber() {
-		return alternateMobileNumber;
-	}
-
-	public void setAlternateMobileNumber(String alternateMobileNumber) {
-		this.alternateMobileNumber = alternateMobileNumber;
-	}
-
 	public LocalDate getDateOfBirth() {
 		return dateOfBirth;
 	}
-
 	public void setDateOfBirth(LocalDate dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
-
-	public String getEmail() {
-		return email;
+	public String getGender() {
+		return gender;
 	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
-
-	public Gender getSex() {
-		return sex;
-	}
-
-	public void setSex(Gender sex) {
-		this.sex = sex;
-	}
-
-	public String getComments() {
-		return comments;
-	}
-
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-
-	public CourseEnum getCourseApplyingFor() {
-		return courseApplyingFor;
-	}
-
-	public void setCourseApplyingFor(CourseEnum courseApplyingFor) {
-		this.courseApplyingFor = courseApplyingFor;
-	}
-
 	public String getHighestQualification() {
 		return highestQualification;
 	}
-
 	public void setHighestQualification(String highestQualification) {
 		this.highestQualification = highestQualification;
 	}
-
-	public ModeOfEnquiry getModeOfEnquiry() {
+	public String getModeOfEnquiry() {
 		return modeOfEnquiry;
 	}
-
-	public void setModeOfEnquiry(ModeOfEnquiry modeOfEnquiry) {
+	public void setModeOfEnquiry(String modeOfEnquiry) {
 		this.modeOfEnquiry = modeOfEnquiry;
 	}
-
-	public EnquiryStatus getStatus() {
-		return status;
-	}
-
-	public void setStatus(EnquiryStatus status) {
-		this.status = status;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 	public LocalDate getEnquiryDate() {
 		return enquiryDate;
 	}
-
 	public void setEnquiryDate(LocalDate enquiryDate) {
 		this.enquiryDate = enquiryDate;
 	}
-
-	public LocalDate getUpdatedOn() {
-		return updatedOn;
+	public String getComments() {
+		return comments;
 	}
-
-	public void setUpdatedOn(LocalDate updatedOn) {
-		this.updatedOn = updatedOn;
+	public void setComments(String comments) {
+		this.comments = comments;
 	}
-
-	public String getUpdatedBy() {
-		return updatedBy;
+	public Long getBranchId() {
+		return branchId;
 	}
-
-	public void setUpdatedBy(String updatedBy) {
-		this.updatedBy = updatedBy;
+	public void setBranchId(Long branchId) {
+		this.branchId = branchId;
 	}
-
-	public Branch getBranch() {
-		return branch;
+	public Long getDepartmentId() {
+		return departmentId;
 	}
-
-	public void setBranch(Branch branch) {
-		this.branch = branch;
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
 	}
-
-	public Department getDepartment() {
-		return department;
+	public Long getCourseId() {
+		return courseId;
 	}
-
-	public void setDepartment(Department department) {
-		this.department = department;
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
 	}
-
-	public Batch getBatch() {
-		return batch;
+	public Long getSemesterId() {
+		return semesterId;
 	}
-
-	public void setBatch(Batch batch) {
-		this.batch = batch;
+	public void setSemesterId(Long semesterId) {
+		this.semesterId = semesterId;
 	}
-
-	public State getState() {
-		return state;
+	public Long getBatchId() {
+		return batchId;
 	}
-
-	public void setState(State state) {
-		this.state = state;
+	public void setBatchId(Long batchId) {
+		this.batchId = batchId;
 	}
-
-	public City getCity() {
-		return city;
+	public Long getStateId() {
+		return stateId;
 	}
-
-	public void setCity(City city) {
-		this.city = city;
+	public void setStateId(Long stateId) {
+		this.stateId = stateId;
 	}
-
-	public Country getCountry() {
-		return country;
+	public Long getCityId() {
+		return cityId;
 	}
-
-	public void setCountry(Country country) {
-		this.country = country;
+	public void setCityId(Long cityId) {
+		this.cityId = cityId;
 	}
-
+	public Long getAcademicYearId() {
+		return academicYearId;
+	}
+	public void setAcademicYearId(Long academicYearId) {
+		this.academicYearId = academicYearId;
+	}
+	public String getEnquiryStatus() {
+		return enquiryStatus;
+	}
+	public void setEnquiryStatus(String enquiryStatus) {
+		this.enquiryStatus = enquiryStatus;
+	}
+	
 	public String getStrDateOfBirth() {
 		return strDateOfBirth;
 	}
-
 	public void setStrDateOfBirth(String strDateOfBirth) {
 		this.strDateOfBirth = strDateOfBirth;
 	}
-
 	public String getStrEnquiryDate() {
 		return strEnquiryDate;
 	}
-
 	public void setStrEnquiryDate(String strEnquiryDate) {
 		this.strEnquiryDate = strEnquiryDate;
 	}
-
-	public String getStrUpdatedOn() {
-		return strUpdatedOn;
+	public List<String> getGenderList() {
+		return genderList;
 	}
-
-	public void setStrUpdatedOn(String strUpdatedOn) {
-		this.strUpdatedOn = strUpdatedOn;
+	public void setGenderList(List<String> genderList) {
+		this.genderList = genderList;
 	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (!(o instanceof CmsAdmissionEnquiryVo))
-			return false;
-		CmsAdmissionEnquiryVo that = (CmsAdmissionEnquiryVo) o;
-		return Objects.equals(getTotalAdmissions(), that.getTotalAdmissions())
-				&& Objects.equals(getTotalFollowup(), that.getTotalFollowup())
-				&& Objects.equals(getTotalDeclined(), that.getTotalDeclined())
-				&& Objects.equals(getTotalConverted(), that.getTotalConverted())
-				&& Objects.equals(getId(), that.getId())
-				&& Objects.equals(getStudentName(), that.getStudentName())
-				&& Objects.equals(getStudentMiddleName(), that.getStudentMiddleName())
-				&& Objects.equals(getStudentLastName(), that.getStudentLastName())
-				&& Objects.equals(getFatherName(), that.getFatherName())
-				&& Objects.equals(getFatherMiddleName(), that.getFatherMiddleName())
-				&& Objects.equals(getFatherLastName(), that.getFatherLastName())
-				&& Objects.equals(getMotherName(), that.getMotherName())
-				&& Objects.equals(getMotherMiddleName(), that.getMotherMiddleName())
-				&& Objects.equals(getMotherLastName(), that.getMotherLastName())
-				&& Objects.equals(getContactNumber(), that.getContactNumber())
-				&& Objects.equals(getAlternateMobileNumber(),
-						that.getAlternateMobileNumber())
-				&& Objects.equals(getDateOfBirth(), that.getDateOfBirth())
-				&& Objects.equals(getEmail(), that.getEmail())
-				&& getSex() == that.getSex()
-				&& Objects.equals(getComments(), that.getComments())
-				&& getCourseApplyingFor() == that.getCourseApplyingFor()
-				&& Objects.equals(getHighestQualification(),
-						that.getHighestQualification())
-				&& getModeOfEnquiry() == that.getModeOfEnquiry()
-				&& getStatus() == that.getStatus()
-				&& Objects.equals(getDescription(), that.getDescription())
-				&& Objects.equals(getEnquiryDate(), that.getEnquiryDate())
-				&& Objects.equals(getUpdatedOn(), that.getUpdatedOn())
-				&& Objects.equals(getUpdatedBy(), that.getUpdatedBy())
-				&& Objects.equals(getBranch(), that.getBranch())
-				&& Objects.equals(getDepartment(), that.getDepartment())
-				&& Objects.equals(getBatch(), that.getBatch())
-				&& Objects.equals(getState(), that.getState())
-				&& Objects.equals(getCity(), that.getCity())
-				&& Objects.equals(getCountry(), that.getCountry())
-				&& Objects.equals(getStrDateOfBirth(), that.getStrDateOfBirth())
-				&& Objects.equals(getStrEnquiryDate(), that.getStrEnquiryDate())
-				&& Objects.equals(getStrUpdatedOn(), that.getStrUpdatedOn());
+	public List<String> getModeOfEnquiryList() {
+		return modeOfEnquiryList;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(getTotalAdmissions(), getTotalFollowup(), getTotalDeclined(),
-				getTotalConverted(), getId(), getStudentName(), getStudentMiddleName(),
-				getStudentLastName(), getFatherName(), getFatherMiddleName(),
-				getFatherLastName(), getMotherName(), getMotherMiddleName(),
-				getMotherLastName(), getContactNumber(), getAlternateMobileNumber(),
-				getDateOfBirth(), getEmail(), getSex(), getComments(),
-				getCourseApplyingFor(), getHighestQualification(), getModeOfEnquiry(),
-				getStatus(), getDescription(), getEnquiryDate(), getUpdatedOn(),
-				getUpdatedBy(), getBranch(), getDepartment(), getBatch(), getState(),
-				getCity(), getCountry(), getStrDateOfBirth(), getStrEnquiryDate(),
-				getStrUpdatedOn());
+	public void setModeOfEnquiryList(List<String> modeOfEnquiryList) {
+		this.modeOfEnquiryList = modeOfEnquiryList;
 	}
-
-	@Override
-	public String toString() {
-		return "CmsAdmissionEnquiryVo{" + "totalAdmissions=" + totalAdmissions
-				+ ", totalFollowup=" + totalFollowup + ", totalDeclined=" + totalDeclined
-				+ ", totalConverted=" + totalConverted + ", id=" + id + ", studentName='"
-				+ studentName + '\'' + ", studentMiddleName='" + studentMiddleName + '\''
-				+ ", studentLastName='" + studentLastName + '\'' + ", fatherName='"
-				+ fatherName + '\'' + ", fatherMiddleName='" + fatherMiddleName + '\''
-				+ ", fatherLastName='" + fatherLastName + '\'' + ", motherName='"
-				+ motherName + '\'' + ", motherMiddleName='" + motherMiddleName + '\''
-				+ ", motherLastName='" + motherLastName + '\'' + ", contactNumber='"
-				+ contactNumber + '\'' + ", alternateMobileNumber='"
-				+ alternateMobileNumber + '\'' + ", dateOfBirth=" + dateOfBirth
-				+ ", email='" + email + '\'' + ", sex=" + sex + ", comments='" + comments
-				+ '\'' + ", courseApplyingFor=" + courseApplyingFor
-				+ ", highestQualification='" + highestQualification + '\''
-				+ ", modeOfEnquiry=" + modeOfEnquiry + ", status=" + status
-				+ ", description='" + description + '\'' + ", enquiryDate=" + enquiryDate
-				+ ", updatedOn=" + updatedOn + ", updatedBy='" + updatedBy + '\''
-				+ ", branch=" + branch + ", department=" + department + ", batch=" + batch
-				+ ", state=" + state + ", city=" + city + ", country=" + country
-				+ ", strDateOfBirth='" + strDateOfBirth + '\'' + ", strEnquiryDate='"
-				+ strEnquiryDate + '\'' + ", strUpdatedOn='" + strUpdatedOn + '\'' + '}';
+	public List<String> getEnquiryStatusList() {
+		return enquiryStatusList;
 	}
+	public void setEnquiryStatusList(List<String> enquiryStatusList) {
+		this.enquiryStatusList = enquiryStatusList;
+	}
+	public String getTransactionSource() {
+		return transactionSource;
+	}
+	public void setTransactionSource(String transactionSource) {
+		this.transactionSource = transactionSource;
+	}
+	public String getSourceOfApplication() {
+		return sourceOfApplication;
+	}
+	public void setSourceOfApplication(String sourceOfApplication) {
+		this.sourceOfApplication = sourceOfApplication;
+	}
+	
+
 }
