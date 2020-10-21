d7193785402be71c3a3ab224a62180a901d6873e
Anju Sharma <sharmanju80@gmail.com>
2020-10-21 12:07:14 -0400

    Update entity with unique costraint

--- src/main/java/com/synectiks/commons/entities/CloudEntity.java
+++ src/main/java/com/synectiks/commons/entities/CloudEntity.java
@@ -3,11 +3,9 @@
  */
 package com.synectiks.commons.entities;
 
-import java.util.List;
-
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.synectiks.commons.constants.IDBConsts;
 
@@ -15,7 +13,9 @@ import com.synectiks.commons.constants.IDBConsts;
  * @author Rajesh
  */
 @Entity
-@Table(name = IDBConsts.Tbl_CLOUD_ENTITY)
+@Table(name = IDBConsts.Tbl_CLOUD_ENTITY,
+		uniqueConstraints={@UniqueConstraint(
+				columnNames = {"cloudName" , "groupName", "entity"})})
 public class CloudEntity extends PSqlEntity {
 
 	private static final long serialVersionUID = 6032668071445564187L;
